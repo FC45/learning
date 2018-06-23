6909bd02d2819320c1ec36fbb726a8d67d817e69
FC <wei01555@gmail.com>
2018-06-23 22:01:33 +0800

    Update remove element

--- c/remove_element.c
+++ c/remove_element.c
@@ -2,13 +2,9 @@
 
 int removeElement(int* nums, int numsSize, int val) {
     int* current = nums;
-    int len = 0;
     for (int i = 0; i < numsSize; i++) {
-        if (nums[i] != val) {
-            *current = nums[i];
-            current++;
-            len++;
-        }
+        if (nums[i] != val)
+            *current++ = nums[i];
     }
-    return len;
+    return current-nums;
 }
